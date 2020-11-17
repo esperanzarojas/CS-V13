cafd5a56bcc81af74d51385f0cf7ee874bbc4f97
esperanzarojas <rojasesperanza11@gmail.com>
2020-11-17 02:05:26 +0000

    working

---
+++ question3.cpp
@@ -0,0 +1,32 @@
+#include    <iostream>
+#include    <ctime>
+#include    <cstdlib>
+using namespace std;
+void makebinary(int [], int);
+void printbinary(int [], int);
+void count0cluster(int [], int);
+int main()
+{
+    const int SIZE = 20;
+    int     binary[SIZE];
+
+    srand(time(0));
+    makebinary(binary, SIZE);
+    printbinary(binary, SIZE);
+    count0cluster(binary, SIZE);
+} //endl
+void makebinary(int num[], int size)
+{
+    for(int i=0;i<size; i++)
+        num[i] = rand() % 2;
+}
+void printbinary(int num[], int size)
+{
+    for(int i=0;i<size; i++)
+        cout << num[i] << " ";
+    cout << endl;
+}
+void count0cluster(int bin[], int size)
+{
+  int c = 0, side = 1;
+}
